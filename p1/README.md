c8d777c49718c0b084a08fa7bf6ec890f0a935d8
Phil Yuska <philyuska@gmail.com>
2019-09-14 13:54:59 -0400

    Initial commit of p1.

---
+++ p1/README.md
@@ -0,0 +1,9 @@
+# Project 1
++ By: *Phil Yuska*
++ Production URL: <http://p1.tymabus.fun>
+
+## Outside resources
+
+
+## Notes for instructor
+
---
+++ p1/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+
+    <title>Clown Princess Abigail</title>
+    <meta charset='utf-8'>
+
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <style>
+	
+		body
+		{
+			font-family: Arial, Helvetica, sans-serif;
+		}
+
+        .container
+		{
+            width: 500px;
+            margin: auto;
+            text-align: center;
+        }
+
+        img
+		{
+            width: 400px;
+			height: 500px; 
+        }
+
+		p
+		{
+			align: justify;
+		}
+		
+	</style>
+
+</head>
+
+<body>
+
+    <div class='container'>
+
+        <h1>Clown Princess Abigail</h1>
+
+        <img src='images/abby.jpg' alt='Photo of Clown Princess Abigail'>
+
+        <h2>About Me</h2>
+		
+        <p>
+            I currently live in Winchester MA but I am originally from the mean streets of Tennessee. A nice lady named Sue rescued me after I showed up unprepared to a gun fight. She fixed me all up and found me a new place to call home. "God bless you Sue."
+		</p>
+
+        <p>
+			I graduated from the K-9 East Dog Training School in 2015. I don't like cats, rabbits, squirrels or strangers so if you see me on the street, I caution against you approaching me unless you have a ham sandwich or something else good to eat.
+        </p>
+		
+    </div>
+	
+</body>
+
+</html>
