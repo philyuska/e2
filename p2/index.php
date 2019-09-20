b0333f53838e6a2ea75f211c988afe7a039fe4e7
Phil Yuska <philyuska@gmail.com>
2019-09-20 04:22:00 -0400

    updated p2/index.php with page framework

---
+++ p2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+
+    <title>project 2</title>
+    <meta charset='utf-8'>
+
+</head>
+
+<body>
+
+    <h1>[ game title ]</h1>
+
+    <h2>Mechanics</h2>
+    <ul>
+        <li>step x.</li>
+        <li>step y.</li>
+        <li>step z.</li>
+    </ul>
+
+    <h2>Results</h2>
+
+</body>
+
+</html>
