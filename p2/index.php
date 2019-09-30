0f5b622638d9a0d0d68661cb5276c330151cf272
Phil Yuska <philyuska@gmail.com>
2019-09-30 04:23:53 -0400

    Update index.php with proper title

--- p2/index.php
+++ p2/index.php
@@ -4,7 +4,7 @@
 
 <head>
 
-    <title>Practice project 2</title>
+    <title>Blackjack Game Simulator</title>
     <meta charset='utf-8'>
 
     <meta name='viewport' content='width=device-width, initial-scale=1'>
