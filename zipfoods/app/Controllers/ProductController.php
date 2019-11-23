34618ec960c05bcfd80d57df98fd1f8c095ad213
Phil Yuska <philyuska@gmail.com>
2019-11-23 06:12:21 -0500

    Ketchup zipfoods to week 11 minus quiz

--- zipfoods/app/Controllers/ProductController.php
+++ zipfoods/app/Controllers/ProductController.php
@@ -1,39 +1,84 @@
 <?php
 namespace App\Controllers;
 
-use App\Products;
-
 class ProductController extends Controller
 {
     private $products;
-
+    /**
+     *
+     */
     public function __construct($app)
     {
         parent::__construct($app);
-        $this->products = new Products($this->app->path('database/products.json'));
     }
-
+    /**
+     * Listing of all products
+     * /products
+     */
     public function index()
     {
         return $this->app->view('products.index', [
-        'products' => $this->products->getAll()
+            'products' => $this->app->db()->all('products')
         ]);
     }
-
+    /**
+     * Show an individual product
+     * /product?id={id}
+     */
     public function show()
     {
+        # Get the `id` route parameter
         $id = $this->app->param('id');
-
-        $product = $this->products->getById($id);
-
-        if (is_null($product)) {
-            return $this->app->view('products.missing', [
-                'id' => $id
-            ]);
+        # If no id is present, send the user to the products page
+        if (is_null($id)) {
+            $this->app->redirect('/products');
         }
+        # Load the product details
+        $product = $this->app->db()->findById('products', $id);
+        # If we couldn't find the product, return the "product missing" view
+        if (is_null($product)) {
+            return $this->app->view('products.missing', ['id' => $id]);
+        }
+        # Load the review details
+        $reviews = $this->app->db()->findByColumn('reviews', 'product_id', '=', $id);
+        # If the user submitted the review form, we'll have a confirmation name
+        # that we'll pass to the view to show them a confirmation message
+        $confirmationName = $this->app->old('confirmationName');
         
         return $this->app->view('products.show', [
-            'product' => $product
+            'product' => $product,
+            'reviews' => $reviews,
+            'confirmationName' => $confirmationName
         ]);
+    }
+    /**
+     * Process the review form from the product page
+     * /products/save-review
+     */
+    public function saveReview()
+    {
+        $this->app->validate([
+            'name' => 'required',
+            'content' => 'required|minLength:200',
+        ]);
+        # If the above validation fails, the user is redirected back to the product page
+        # and none of the following code will execute
+        
+        # Extract data from the form submission
+        $name = $this->app->input('name');
+        $content = $this->app->input('content');
+        $id = $this->app->input('id');
+        
+        # Insert into the database
+        $data = [
+            'name' => $name,
+            'content' => $content,
+            'product_id' => $id,
+        ];
+        $this->app->db()->insert('reviews', $data);
+        
+        # Send the user back to the product page with a `confirmationName`
+        # we'll use to display a confirmation message.
+        $this->app->redirect('/product?id='.$id, ['confirmationName' => $name]);
     }
 }
--- zipfoods/routes.php
+++ zipfoods/routes.php
@@ -4,5 +4,7 @@ return [
     '/' => ['AppController', 'index'],
     '/products' => ['ProductController', 'index'],
     '/product' => ['ProductController', 'show'],
+    '/products/save-review' => ['ProductController', 'saveReview'],
     '/about' => ['AppController', 'about'],
+    '/practice' => ['AppController', 'practice'],
 ];
